c597c75c937e07ee18db82cf812a2cd685b1fff2
Hanif Prasetyo <hanifp@hewlett>
2025-12-25 10:51:26 +0700

    memperbarui tampilan untuk mobile

--- API/verify_profile_deletion_draft.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-import os
-
-BASE_URL = "http://localhost:3000" # Assuming the API is running on localhost:3000. Wait, I should check the port.
-# I'll check main.py or config.py to be sure. I'll make the script flexible or check first.
-# From previous interactions, API usually runs on 3000? No, standard FastAPI is 8000.
-# I will check main.py first.
-
